6416f76a05f9561f486177e44402eeec64b13f20
Francisco Ponce <francisco.ponce.dev@gmail.com>
2022-07-05 16:45:55 -0300

    linters

--- app/controllers/v1/messages_controller.rb
+++ app/controllers/v1/messages_controller.rb
@@ -1,8 +1,12 @@
-class V1::MessagesController < ApplicationController
-  def index
-    @message = Message.find(Message.pluck(:id).sample)
-    render json: {
-      message: @message
-    }
+# frozen_string_literal: true
+
+module V1
+  class MessagesController < ApplicationController
+    def index
+      @message = Message.find(Message.pluck(:id).sample)
+      render json: {
+        message: @message
+      }
+    end
   end
 end
